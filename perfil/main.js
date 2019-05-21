d0ec1eaa9d5d5a3709c0f65397f61575a93939f7
pauqua <paulaquaretti@gmail.com>
2019-05-20 21:47:31 -0300

    cambios perfil

--- perfil/main.js
+++ perfil/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#button-post").click(function() {
         $("#muro").append(
-          '<div class="mi-post card">'+ 
+          '<div class="mi-post card col-12 col-lg-12 offset-3 offset-lg-0">'+ 
         
           '<div class="card-body">'+
     
