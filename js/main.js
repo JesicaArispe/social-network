c30308832e595b3e3c82a01124463b5b705c51e0
JesicaArispe <jess.arispe@gmail.com>
2019-05-20 21:49:35 -0300

    cambios en home

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
 
-    $("#button-post").click(function() {
+    $("#button-post").click(function () {
         $("#post").append(
-            '<div class="post-estado">'+
-            '<input class="form-control" type="text" placeholder="Post Estado">'+
-            '</div>'
-                 )
+            '<div class="post-estado">' +
+            // '<input class="form-control" id="postear" type="text" placeholder="Post Estado">' +
+            '</div>' +
+            $("#postear").val() +
+            '<ul class="nav nav-pills nav-justified">' +
+            '<li class="nav-item">' +
+            '<a class="nav-link" href="#!">Me gusta</a>' +
+            '</li>' +
+            '<li class="nav-item">' +
+            '<a class="nav-link" href="#!">Comentar</a>' +
+            '</li>' +
+            '<li class="nav-item">' +
+            '<a class="nav-link" href="#!">Compartir</a>' +
+            '</li>' +
+            '</ul>' +
+            '<br></br>'
+
+        )
     })
-            
+
 })
