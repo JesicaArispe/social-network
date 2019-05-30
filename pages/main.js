b74daa3fc8659125e4eb8f0374b24bc260c25708
pauqua <paulaquaretti@gmail.com>
2019-05-30 17:13:33 -0300

    cambios perfil

---
+++ pages/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $(".btn-login").click(function() {
+        
+        var usuario = "paula";
+        var inputUsuario = $(".usuario").val();
+
+    if ("inputUsuario"==="usuario") {
+        alert ("correcto")}
+
+    });
+});
