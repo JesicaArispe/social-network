e24d9ae472b1b91944ef118074ac6f0dcf891e88
pauqua <paulaquaretti@gmail.com>
2019-06-06 20:43:43 -0300

    cambios perfil

--- perfil/main.js
+++ perfil/main.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
     $("#button-post").click(function() {
         $("#muro").append(
-          '<div class="mi-post card col-12 col-lg-12 offset-3 offset-lg-0">'+ 
+        '<div>'+
+          '<div class="mi-post card col-12 col-lg-12 offset-3 offset-lg-0">' + 
         
-          '<div class="card-body">'+
+             '<div class="card-body">' +
     
-          '<p class="card-text">'+
-          $("#post-personal").val()+
-          '</p>'+
+               '<p class="card-text">'+
+                $("#post-personal").val()+
+              '</p>' +
+
+            '</div>' +
           '</div>'+
-          '</div>' +
-          '<button id="eliminar" type="button" class="btn btn-outline-warning">eliminar</button>'
+            '<button id="eliminar" type="button" class="col-2 offset-10 btn btn-outline-warning">eliminar</button>' +
+        '</div>'
         );
     });
   });
